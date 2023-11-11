cc719b8ad8360ee2ba4ebc0fc11ca026cdd63f45
Takayuki Ono <takaaaaadayo@gmail.com>
2023-11-11 23:28:40 +0530

    success for call backend

--- frontend/src/components/CreatePage.tsx
+++ frontend/src/components/CreatePage.tsx
@@ -3,28 +3,47 @@ import axios from 'axios';
 
 const CreatePage = () => {
   const [prompt, setPrompt] = useState('');
-  const [images, setImages] = useState<string[]>([]);
+  const [imageUrl, setImageUrl] = useState('');
+  const [error, setError] = useState('');
 
-  const handleGenerateBook = async () => {
-    // Here you would call the backend API to generate images
-    // For this example, we'll mock the API call
-    const generatedImages = await axios.post('/api/generate-book', { prompt });
-    setImages(generatedImages.data);
+  const handleGenerateImage = async () => {
+    setError(''); // Clear any previous errors
+    if (!prompt) {
+      setError('Please enter a prompt.');
+      return;
+    }
+    
+    try {
+      // Call the backend API to generate images
+      const response = await axios.post('http://127.0.0.1:5000/api/generate-image', { prompt });
+      console.log('Response:', response);
+      setImageUrl(response.data.url); // Assuming the backend responds with { url: 'image_url' }
+    } catch (error) {
+      console.error('Error generating image:', error);
+      setError('Failed to generate image. Please try again later.');
+    }
   };
 
   return (
-    <div className="create-page">
+    <div className="create-page p-6">
       <textarea
+        className="textarea w-full h-32 p-2 border rounded mb-4"
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
         placeholder="Enter prompts for your picture-book..."
       />
-      <button onClick={handleGenerateBook}>Generate Picture-Book</button>
-      <div className="image-gallery">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt={`Page ${index + 1}`} />
-        ))}
-      </div>
+      <button
+        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleGenerateImage}
+      >
+        Generate Image
+      </button>
+      {error && <p className="text-red-500">{error}</p>}
+      {imageUrl && (
+        <div className="image-gallery mt-4">
+          <img src={imageUrl} alt="Generated" className="rounded shadow-lg max-w-full h-auto" />
+        </div>
+      )}
     </div>
   );
 };
